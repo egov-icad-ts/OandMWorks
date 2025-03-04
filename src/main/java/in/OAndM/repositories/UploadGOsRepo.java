51108d65b39b9c82376b024e458bd6516e6b20fc
Navya Kanikaram <navyakanikaram@gmail.com>
2025-03-04 15:10:05 +0530

    Sort Gos by date

--- src/main/java/in/OAndM/repositories/UploadGOsRepo.java
+++ src/main/java/in/OAndM/repositories/UploadGOsRepo.java
@@ -2,13 +2,13 @@ package in.OAndM.repositories;
 
 import java.util.List;
 
-import in.OAndM.Entities.AgreementsEntity;
+import org.springframework.data.domain.Sort;
 import in.OAndM.Entities.UploadGOsEntity;
 import in.OAndM.core.BaseRepository;
 
 public interface UploadGOsRepo extends  BaseRepository<UploadGOsEntity, Integer>{
 	
-	public List<UploadGOsEntity> findByuploadTypeAndDeleteFlagFalse(String type);
+	public List<UploadGOsEntity> findByuploadTypeAndDeleteFlagFalse(String type,Sort sort);
 
 }
 
--- src/main/java/in/OAndM/services/impl/UploadGosServiceImpl.java
+++ src/main/java/in/OAndM/services/impl/UploadGosServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Sort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -47,7 +48,8 @@ public class UploadGosServiceImpl  extends BaseServiceImpl<UploadGOsEntity, Uplo
 		BaseResponse<HttpStatus, List<UploadGOsModel>> responseJson = new BaseResponse<>();
 		List<UploadGOsEntity> list;
 		List<UploadGOsModel> models = new ArrayList<>();
-		list = gosRepo.findByuploadTypeAndDeleteFlagFalse(type);
+		 Sort sort = Sort.by(Sort.Order.desc("goDt"));
+		list = gosRepo.findByuploadTypeAndDeleteFlagFalse(type,sort);
 		for(UploadGOsEntity en : list) {
 			UploadGOsModel model = new UploadGOsModel();
 			model.setGoAmount(en.getGoAmount());
