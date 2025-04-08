477f40881d8e3fdbf1898512127089138bf26f7d
Lavanya <sukkalavanya@gmail.com>
2025-04-08 16:57:50 +0530

    WEBCONFIG CHNAGES

--- src/main/java/in/OAndM/config/WebConfig.java
+++ src/main/java/in/OAndM/config/WebConfig.java
@@ -10,13 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		  registry.addMapping("/**")  // Allow CORS for all endpoints
-          
-          .allowedOrigins("http://localhost:3002", "http://192.168.1.122:3002","http://localhost:3000")// Allow requests from the front-end app
-          .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // Allowed HTTP methods
-          .allowedHeaders("*") // Allow all headers
-          .allowCredentials(true); // Allow credentials like cookies or authorization headers
-	}
+	/*
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**") // Allow CORS for all endpoints
+	 * 
+	 * .allowedOrigins("http://localhost:3002",
+	 * "http://192.168.1.122:3002","http://localhost:3000")// Allow requests from
+	 * the front-end app .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
+	 * // Allowed HTTP methods .allowedHeaders("*") // Allow all headers
+	 * .allowCredentials(true); // Allow credentials like cookies or authorization
+	 * headers }
+	 */
 }
