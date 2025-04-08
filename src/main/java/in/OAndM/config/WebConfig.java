57b1145c201d546bab44bb35bf5bf9c757512891
Lavanya <sukkalavanya@gmail.com>
2025-04-08 11:41:37 +0530

    webconfig change commit

--- src/main/java/in/OAndM/config/WebConfig.java
+++ src/main/java/in/OAndM/config/WebConfig.java
@@ -10,12 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
-				.allowedOrigins("http://localhost:3000", "http://192.168.1.122:3002", "http://localhost:3002","http://192.168.1.122:3002"); // React
-																												// app
-																												// URL }
-
-	}
+	/*
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**").allowedMethods("OPTIONS", "GET", "POST", "PUT",
+	 * "DELETE") .allowedOrigins("http://localhost:3000",
+	 * "http://192.168.1.122:3002",
+	 * "http://localhost:3002","http://192.168.1.122:3002"); // React // app // URL
+	 * }
+	 * 
+	 * }
+	 */
 }
