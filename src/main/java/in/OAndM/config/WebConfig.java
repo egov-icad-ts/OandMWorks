93b0bc239b6e65e9cd2ce278c83ba00203fc44e1
Lavanya <sukkalavanya@gmail.com>
2025-04-17 17:14:39 +0530

    build commit

--- src/main/java/in/OAndM/config/WebConfig.java
+++ src/main/java/in/OAndM/config/WebConfig.java
@@ -12,13 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
 
 	/*
 	 * @Override public void addCorsMappings(CorsRegistry registry) {
-	 * registry.addMapping("/**") // Allow CORS for all endpoints
-	 * 
-	 * .allowedOrigins("http://localhost:3002",
+	 * registry.addMapping("/**").allowedOrigins("http://localhost:3002",
 	 * "http://192.168.1.122:3002","http://localhost:3000")// Allow requests from
-	 * the front-end app .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
-	 * // Allowed HTTP methods .allowedHeaders("*") // Allow all headers
-	 * .allowCredentials(true); // Allow credentials like cookies or authorization
-	 * headers }
+	 * .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // Allowed HTTP
+	 * methods .allowedHeaders("*") // Allow all headers .allowCredentials(true); //
+	 * Allow credentials like cookies or authorization }
 	 */
+
 }
